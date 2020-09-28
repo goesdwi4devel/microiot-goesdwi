a6242a98d2c7d19633007d04dacdbc3623f32b7a
Goes Dwi <goesdwi4devel@gmail.com>
2020-09-28 21:38:55 +0800

    Code Tugas Week 1

--- code-task-week1/src/main.cpp
+++ code-task-week1/src/main.cpp
@@ -1,9 +1,60 @@
 #include <Arduino.h>
 
-void setup() {
+#define LED1 14
+#define LED2 4
+#define LED3 5
+#define LED4 16
+String readString;
+
+void setup()
+{
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  if (Serial.available())
+  {
+    char data = Serial.read();
+    readString += data;
+    Serial.print("Karakter yang dikirim: ");
+    Serial.println(readString);
+
+    if (readString == "1")
+    {
+      Serial.println("LED1 On");
+      digitalWrite(LED1, HIGH);
+      delay(2000);
+      digitalWrite(LED1, LOW);
+    }
+    else if (readString == "2")
+    {
+      Serial.println("LED2 On");
+      digitalWrite(LED2, HIGH);
+      delay(2000);
+      digitalWrite(LED2, LOW);
+    }
+    else if (readString == "3")
+    {
+      Serial.println("LED3 On");
+      digitalWrite(LED3, HIGH);
+      delay(2000);
+      digitalWrite(LED3, LOW);
+    } else if (readString == "4")
+    {
+      Serial.println("LED4 On");
+      digitalWrite(LED4, HIGH);
+      delay(2000);
+      digitalWrite(LED4, LOW);
+    } else {
+      Serial.println("Masukkan nilai 1 - 4");
+    }
+    readString = "";
+    Serial.println("");
+  }
 }
